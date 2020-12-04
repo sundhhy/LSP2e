f514f39fe7f0a9950167a7c7f1a8ac732b79e6b6
sundh <sundhhy@163.com>
2020-12-04 08:00:53 +0800

    增加了获取文件大小和类型的例子

---
+++ chap08/stat_8_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    struct stat sb;
+    int ret;
+    if (argc < 2)
+    {
+        fprintf(stderr, "usage:%s <file>\n", argv[0]);
+        return 1; 
+    }
+
+    ret = stat(argv[1], &sb);
+    if (ret) {
+        perror("stat");
+        return 1;
+    }
+
+    printf("%s is %ld bytes\n", argv[1], sb.st_size);
+	return 0;
+
+}
